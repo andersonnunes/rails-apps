f2fbfe838caf71fb25586907dcedb0165441bfba
Anderson Nunes <anderson.nunes1988@gmail.com>
2017-08-04 22:21:39 -0300

    FileCabinet - First View File for Doc Model

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -6,9 +6,17 @@ class DocsController < ApplicationController
     end
 
     def new
+        @doc = Doc.new
     end
 
     def create
+        @doc = Doc.new(doc_params)
+
+        if @doc.save
+            redirect_to @doc
+        else
+            render 'new'
+        end
     end
 
     def edit
@@ -25,6 +33,7 @@ class DocsController < ApplicationController
         def find_doc
         end
 
-        def docs_params
+        def doc_params
+            params.require(:doc).permit(:title, :content)
         end
 end
